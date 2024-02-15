0a7ec9659e3dfc49e07c70ac2ba644ac7339410a
baldijm <bxldijm@gmail.com>
2024-02-15 16:19:57 -0600

    navbar commit stroke

--- web/components/ui/ui-layout.tsx
+++ web/components/ui/ui-layout.tsx
@@ -29,7 +29,7 @@ export function UiLayout({ children }: { children: ReactNode }) {
         <div className="navbar glassmorphism">
   <div className="flex-none">
     <button className="btn btn-square btn-ghost">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-navbar"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
     </button>
   </div>
   <div className="flex-1">
