99290b78f76b427b75a16369cfbaae0656a064c3
baldijm <bxldijm@gmail.com>
2024-02-14 22:16:47 -0600

    a

--- web/components/ui/ui-layout.tsx
+++ web/components/ui/ui-layout.tsx
@@ -25,33 +25,6 @@ export function UiLayout({ children }: { children: ReactNode }) {
 
   return (
     <div className="h-full flex flex-col">
-      <div className="navbar bg-base-300 text-neutral-content flex-col md:flex-row space-y-2 md:space-y-0">
-        <div className="flex-1">
-          <Link className="btn btn-ghost normal-case text-xl" href="/">
-            <img
-              className="h-4 md:h-6"
-              alt="Solana Logo"
-              src="/solana-logo.png"
-            />
-          </Link>
-          <ul className="menu menu-horizontal px-1 space-x-2">
-            {pages.map(({ label, path }) => (
-              <li key={path}>
-                <Link
-                  className={pathname.startsWith(path) ? 'active' : ''}
-                  href={path}
-                >
-                  {label}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="flex-none space-x-2">
-          <WalletButton />
-          <ClusterUiSelect />
-        </div>
-      </div>
       <ClusterChecker>
         <AccountChecker />
       </ClusterChecker>
