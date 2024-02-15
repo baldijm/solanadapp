1d180d99a4c636f46a09f3d504fb2168ecc51b22
baldijm <bxldijm@gmail.com>
2024-02-15 15:00:40 -0600

    commit

--- web/components/ui/ui-layout.tsx
+++ web/components/ui/ui-layout.tsx
@@ -26,7 +26,7 @@ export function UiLayout({ children }: { children: ReactNode }) {
 
   return (
     <div className="h-full flex flex-col">
-        <div className="navbar bg-white">
+        <div className="navbar glassmorphism">
   <div className="flex-none">
     <button className="btn btn-square btn-ghost">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
