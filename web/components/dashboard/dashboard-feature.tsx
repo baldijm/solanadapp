6a8c8f3921d38f9d55f7b87009a6a95644f4e657
baldijm <bxldijm@gmail.com>
2024-02-28 14:05:52 -0600

    commit

--- web/components/dashboard/dashboard-feature.tsx
+++ web/components/dashboard/dashboard-feature.tsx
@@ -17,10 +17,7 @@ export default function DashboardFeature() {
   return (
     <div className='bg-dashboard'>
             <div className="w-full h-80 relative stickyy">
-              <video className="w-full h-full object-cover absolute top-0 left-0" autoPlay loop muted>
-                <source src="alien.mp4" type="video/mp4" />
-                Tu navegador no soporta el elemento de video.
-              </video>
+            <div class="bounce"></div>
             </div>
     </div>
   );
